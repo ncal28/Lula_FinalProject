9f753d316ef5a76350b46d27645bf7956951327e
forandrea <156220661+forandrea@users.noreply.github.com>
2025-04-24 11:23:09 -0400

    Add project files.

---
+++ Lula_FinalProject/index.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Lula_FinalProject.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="padding:20px;">
+            <h2>Find Median of Two Sorted Arrays</h2>
+
+            <label>Array 1 (comma separated):</label><br />
+            <asp:TextBox ID="txtNums1" runat="server" Width="300px" /><br /><br />
+
+            <label>Array 2 (comma separated):</label><br />
+            <asp:TextBox ID="txtNums2" runat="server" Width="300px" /><br /><br />
+
+            <asp:Button ID="btnCalculate" runat="server" Text="Calculate Median" OnClick="btnCalculate_Click" /><br /><br />
+
+            <asp:Label ID="lblResult" runat="server" Font-Bold="true" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lula_FinalProject/index.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using leetcode_problem_04;
+
+namespace Lula_FinalProject
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int[] nums1 = txtNums1.Text.Split(',').Select(int.Parse).ToArray();
+                int[] nums2 = txtNums2.Text.Split(',').Select(int.Parse).ToArray();
+
+                Problem4 solver = new Problem4();
+                double median = solver.FindMedianSortedArrays(nums1, nums2);
+
+                lblResult.Text = "Median is: " + median;
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = "Error: " + ex.Message;
+            }
+        }
+    }
+}
